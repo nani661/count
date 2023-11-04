4c97ba101c3b12bb28029a0eec1fba1b61948d24
nani661 <115551865+nani661@users.noreply.github.com>
2023-11-04 14:51:48 +0700

    Update main.js

--- main.js
+++ main.js
@@ -3,36 +3,34 @@ const cl2 = document.querySelector(".collum2");
 const cl3 = document.querySelector(".collum3");
 const timeNow = document.querySelector(".timeNow");
 const allDay = document.querySelector(".allDay");
-var today = new Date();
-var countDatee = new Date("January 22, 2024 00:00:00").getTime();
-setInterval(() => {
-  let a = Math.floor(Math.random() * 30);
-  let b = Math.floor(Math.random() * 30);
-  let c = Math.floor(Math.random() * 30);
-  cl1.style.height = `${a}em`;
-  cl2.style.height = `${b}em`;
-  cl3.style.height = `${c}em`;
+
+function countdownToTet() {
+  var countDate = new Date("2024-01-22T00:00:00Z").getTime(); // Đặt ngày Tết
+  var now = new Date().getTime();
+  var gap = countDate - now;
+
+  if (gap <= 0) {
+    // Tết đã đến hoặc đã qua
+    timeNow.innerHTML = "Chúc mừng năm mới!";
+    allDay.innerHTML = "0 ngày";
+    return;
+  }
 
   var second = 1000;
   var minute = second * 60;
   var hour = minute * 60;
   var day = hour * 24;
-  var now = new Date().getTime();
-  var gap = countDatee - now;
 
-  let s = Math.floor((gap % minute) / second);
-  let m = Math.floor((gap % hour) / minute);
-  let h = Math.floor((gap % day) / hour);
-  let d = Math.floor(gap / day);
-  let newYear = new Date(25);
-  timeNow.innerHTML = `${h <= 9 ? "0" : ""}${h}:${m <= 9 ? "0" : ""}${m}:${
-    s <= 9 ? "0" : ""
-  }${s}`;
+  var s = Math.floor((gap % minute) / second);
+  var m = Math.floor((gap % hour) / minute);
+  var h = Math.floor((gap % day) / hour);
+  var d = Math.floor(gap / day);
+
+  timeNow.innerHTML = `${h <= 9 ? "0" : ""}${h}:${m <= 9 ? "0" : ""}${m}:${s <= 9 ? "0" : ""}${s}`;
   allDay.innerHTML = `${d} ngày`;
-}, 1000);
+}
 
-var datem =
-  today.getDate() + "-" + (today.getMonth() + 1) + "-" + today.getFullYear();
-// console.log(datem);
+setInterval(countdownToTet, 1000);
+countdownToTet(); // Gọi hàm này ngay khi trang web được tải lần đầu.
 
-// code tham khao
+var datem = today.getDate() + "-" + (today.getMonth() + 1) + "-" + today.getFullYear();
