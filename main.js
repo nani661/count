2b12c6f45349609eae7a88098b9d91a7ea1e8c3a
nani661 <115551865+nani661@users.noreply.github.com>
2023-11-04 14:55:50 +0700

    Update main.js

--- main.js
+++ main.js
@@ -4,10 +4,18 @@ const cl3 = document.querySelector(".collum3");
 const timeNow = document.querySelector(".timeNow");
 const allDay = document.querySelector(".allDay");
 
-function countdownToTet() {
-  var countDate = new Date("2024-01-22T00:00:00Z").getTime(); // Đặt ngày Tết
-  var now = new Date().getTime();
-  var gap = countDate - now;
+function countdownToLunarNewYear() {
+  // Tính ngày Tết âm lịch
+  const year = 2024; // Năm Âm lịch 2024 (Bính Thân)
+  const lunarDate = LunarDate.fromGregorianDate(new Date(year, 1, 1)); // Chuyển đổi ngày Dương lịch thành Âm lịch
+  const lunarYear = lunarDate.lunarYear;
+  const lunarMonth = lunarDate.lunarMonth;
+  const lunarDay = lunarDate.lunarDay;
+
+  const tetDate = new Date(year, lunarMonth - 1, lunarDay); // Ngày Tết Âm lịch
+
+  const now = new Date();
+  const gap = tetDate.getTime() - now.getTime();
 
   if (gap <= 0) {
     // Tết đã đến hoặc đã qua
@@ -16,21 +24,19 @@ function countdownToTet() {
     return;
   }
 
-  var second = 1000;
-  var minute = second * 60;
-  var hour = minute * 60;
-  var day = hour * 24;
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
 
-  var s = Math.floor((gap % minute) / second);
-  var m = Math.floor((gap % hour) / minute);
-  var h = Math.floor((gap % day) / hour);
-  var d = Math.floor(gap / day);
+  const s = Math.floor((gap % minute) / second);
+  const m = Math.floor((gap % hour) / minute);
+  const h = Math.floor((gap % day) / hour);
+  const d = Math.floor(gap / day);
 
   timeNow.innerHTML = `${h <= 9 ? "0" : ""}${h}:${m <= 9 ? "0" : ""}${m}:${s <= 9 ? "0" : ""}${s}`;
   allDay.innerHTML = `${d} ngày`;
 }
 
-setInterval(countdownToTet, 1000);
-countdownToTet(); // Gọi hàm này ngay khi trang web được tải lần đầu.
-
-var datem = today.getDate() + "-" + (today.getMonth() + 1) + "-" + today.getFullYear();
+setInterval(countdownToLunarNewYear, 1000);
+countdownToLunarNewYear(); // Gọi hàm này ngay khi trang web được tải lần đầu.
